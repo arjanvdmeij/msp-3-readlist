c56dde7a4e793ad69a634905219c17d6f50445aa
Arjan van der Meij <a.vandermeij@gmail.com>
2018-11-17 20:38:09 +0000

    Many changes done to various parts of the styling and positioning of items. 	- Main page contains image and options to sign or sign in 	- Sign up and sign in pages have been given the same image as index page 	- User home page has been added based on session id 	- User page has a log out button. 		- Buttons to be added in same style for the index, sign-up and sign-in pages 	- Add Comics page has been rerouted to live in the user's page, keeping things in one place 		- Here more pages are to be added for viewing and managing of the list 	- List of new comics has been overhauled to include a button to add to the list 		- List is not populating yet, just printing a title. This is next item on the list

--- app.py
+++ app.py
@@ -4,88 +4,121 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 app = Flask(__name__)
-app.secret_key = config.secret_key
+
 
 app.config["MONGO_DBNAME"] = config.mongo['db']
 app.config["MONGO_URI"] = config.mongo['uri']
 
+
 mongo = PyMongo(app)
 
-def refresh_comics():
-    new_comics = mongo.db.new_comics
-    new_comics_list_raw = requests.get(config.marvel_url)
-    new_comics_list_full_tree = new_comics_list_raw.json()
-    new_comics_short_tree = new_comics_list_full_tree['data']['results']
-
-    new_comics.delete_many({})
-    x=0
-    while x < len(new_comics_short_tree):
-        comic_id = new_comics_short_tree[x]['id']
-        comic_title = new_comics_short_tree[x]['title']
-        comic_image_non_ssl = new_comics_short_tree[x]['thumbnail']['path']\
-        + '/portrait_large.'\
-        + new_comics_short_tree[x]['thumbnail']['extension']
-        comic_image_strip_protocol = comic_image_non_ssl.split('//')
-        comic_image = 'https://' + comic_image_strip_protocol[1]
-        new_entry = { "comic_id" : comic_id, "comic_title" : comic_title,
-                    "comic_image" : comic_image }
-        new_comics.insert_one(new_entry)
-        x+=1
         
 @app.route('/')
+@app.route('/home')
 def index():
+    if 'user' in session:
+        return redirect(url_for('user_home'))
     return render_template('index.html')
+
+    
+@app.route('/user_home')
+def user_home():
+    if 'user' in session:
+        return render_template('user_home.html')
+    else:
+        return render_template('index.html')
+
     
 @app.route('/add_comics')
 def add_comics():
     return render_template('add_comics.html',
-    new_comics_list=mongo.db.new_comics.find())
+    new_comics_list=mongo.db.comics.find({ "cw_add_db" : datetime.date.today().strftime("%V%Y")}),
+    old_comics_list=mongo.db.comics.find({ "cw_add_db" : { "$ne" : datetime.date.today().strftime("%V%Y")}}))
     
+@app.route('/add_comic_to_list/<comic_add>')
+def add_comic_to_list(comic_add):
+    print(comic_add)
+    comic_to_add = mongo.db.comics.find_one({ "comic_id" : int(comic_add) })
+    print('\n')
+    print(comic_to_add['comic_title'])
+    print('\n')
+    return redirect('add_comids',code=204) #redirect(url_for('add_comics'))
+    
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    _task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template("edittask.html", task=_task, categories=category_list)
+
+
+
 @app.route('/sign_up', methods=["GET","POST"])
 def sign_up():
     return render_template('sign_up.html')
     
+
 @app.route('/sign_up_submit', methods=["GET","POST"])
 def sign_up_submit():
     users = mongo.db.users
-    if users.find({ "user_name" : request.form["uname"]}):
-        print('\nUsername already exists\n') # fix hier iets mee om form terug te zetten met melding
-        return redirect(url_for('index'))
-    if request.form["pwd"] == request.form["pwd_check"]:
-        pwd_hash = generate_password_hash(request.form['pwd'])
-        print('pwd_hash: ',pwd_hash)
-        user_name = request.form['uname']
-        print('user_name: ',user_name)
-        new_user = { "user_name" : user_name, "pwd" : pwd_hash, "creation_date" : datetime.date.today().strftime("%Y%m%d") }
-        print('new_user: ',new_user)
-        users.insert(new_user)
-        return redirect(url_for('index'))
-    else:
-        print('\n\nIt\'s safe to say that shit went sideways, in a colossal manner..\n\n')
-        return redirect(url_for('sign_up'))
+        
+    if users.find_one({ "user_name" : request.form["uname"]}):
+        print('\nUsername already exists\n')
+        return render_template('sign_up.html', 
+                                u_error = ' - This name is taken, try again')
     
+    if request.form["pwd"] != request.form["pwd_check"]:
+        print('\nPasswords don\'t match\n')
+        return render_template('sign_up.html', 
+                                u_error = '', 
+                                p_error = ' -  Passwords did not match, try again', 
+                                if_pw_error = request.form['uname'])
+        
+    pwd_hash = generate_password_hash(request.form['pwd'])
+    user_name = request.form['uname']
+    session['user'] = request.form['uname']
+    new_user = { "user_name" : user_name, 
+                 "pwd" : pwd_hash, 
+                 "creation_date" : datetime.date.today().strftime("%Y%m%d") }
+    users.insert(new_user)
+    print('\nUser added\n')
+    return redirect(url_for('index'))
+    
+
 @app.route('/sign_in')
 def sign_in():
     return render_template('sign_in.html')
     
+
 @app.route('/sign_in_submit', methods=["POST"])
 def sign_in_submit():
     users=mongo.db.users
     user_name = users.find_one({"user_name" : request.form['uname']})
-    print('\nDatabase pwd string: ',user_name['pwd'])
-    pwd_hash_check = check_password_hash(user_name['pwd'], request.form['pwd'])
-    print('\nForm pwd hash check: ', pwd_hash_check)
+    
+    if not user_name:
+        return render_template('sign_in.html', 
+                                u_error = ' - User does not exist, try again')
     if user_name:
+        pwd_hash_check = check_password_hash(user_name['pwd'], request.form['pwd'])
         if  pwd_hash_check:
-            print('\n\nPerfectly balanced, as all things should be..\n\n')
-            return redirect(url_for('add_comics')) # aanpassen naar user specific page, eerst duidelijk 'inloggen'..
+            session['user'] = request.form['uname']
+            return redirect(url_for('user_home'))
         else:
-            print('\n\nMr. Stark, I don\'t feel so good..\n\n')
-            return redirect(url_for('index'))
+            return render_template('sign_in.html', 
+                                    p_error = ' - Incorrect password, try again',
+                                    if_pw_error = request.form['uname'])
+
+
+@app.route('/log_out')
+def log_out():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
+    app.secret_key = config.secret_key
     app.run(host=os.getenv('IP'), 
-            port=int(os.getenv('PORT')), 
+            port=int(os.getenv('PORT')),
             debug=True)
--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,13 @@
-/* global $ */
+/* global $  M */
 
 $(document).ready(function(){
     $('.collapsible').collapsible();
   });
-  
+
+function disableButton(which,title) {
+    $(which).addClass('disabled');
+    M.toast({html: `${title}<br>Added To List`, 
+    displayLength:1000,
+    classes: 'blue-grey darken-1 white-text'});
+    return;
+}
