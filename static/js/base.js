68359bc9e7b2d48ddbb39ada92ad0c70cc08ea88
Arjan van der Meij <a.vandermeij@gmail.com>
2018-11-18 19:29:10 +0000

    Added toasts into the page for marking and removing comics

--- static/js/base.js
+++ static/js/base.js
@@ -4,10 +4,25 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
   });
 
-function disableButton(which,title) {
+function addToList(which,id,title) {
     $(which).addClass('disabled');
-    M.toast({html: `${title}<br>Added To List`, 
-    displayLength:1000,
-    classes: 'blue-grey darken-1 white-text'});
+    M.toast({html: `Adding:<br><strong>${title}</strong>`, 
+    displayLength:2000,
+    classes: 'blue-grey lighten-5 blue-grey-text'});
+    $(`#${id}`).html(`<i class="material-icons left">check_circle_outline</i>Added`);
+    return;
+}
+
+function markAsRead(title) {
+    M.toast({html: `Marking as read:<br><strong>${title}</strong>`, 
+    displayLength:2000,
+    classes: 'blue-grey lighten-5 blue-grey-text'});
+    return;
+}
+
+function removeFromList(title) {
+    M.toast({html: `Deleting:<br><strong>${title}</strong>`, 
+    displayLength:2000,
+    classes: 'blue-grey lighten-5 blue-grey-text'});
     return;
 }
