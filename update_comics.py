cd08c03920fd1838f7bda9c7574316aef8d013db
Arjan van der Meij <a.vandermeij@gmail.com>
2018-11-21 08:10:49 +0000

    Updated to use the 'onsaleDate' value returned by Marvel API to put into the database rather than the date the script was ran.

--- update_comics.py
+++ update_comics.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-import os, config, requests, json, datetime, pymongo
+import config, requests, json, pymongo
+from datetime import datetime
 
 
 MONGODB_URI = config.mongo['uri']
@@ -16,30 +17,37 @@ def mongo_connect(url):
 
 
 conn = mongo_connect(MONGODB_URI)
-new_comics = conn[DBS_NAME][COLLECTION_NAME]
-cw_add_db = datetime.date.today().strftime("%V%Y")
+comics = conn[DBS_NAME][COLLECTION_NAME]
 
 
 def main():
-    if new_comics.find({ "cw_add_db" : cw_add_db }):
-        print('Abort, comics are already added')
-    else:
-        # connect to marvel API and get comics released for this week
-        new_comics_list_raw = requests.get(config.marvel_url)
-        new_comics_list_full_tree = new_comics_list_raw.json()
-        new_comics_short_tree = new_comics_list_full_tree['data']['results']
+    check_against = comics.find()
+    check_list = [i['comic_id'] for i in check_against]
     
-        #new_comics.delete_many({})  #--------- Commented, no need to remove all, but leave just in case
-        x=0
-        while x < len(new_comics_short_tree):
+    new_comics_list_raw = requests.get(config.marvel_url)
+
+    new_comics_list_full_tree = new_comics_list_raw.json()
+    new_comics_short_tree = new_comics_list_full_tree['data']['results']
+
+    x=0
+    while x < len(new_comics_short_tree):
+        if not new_comics_short_tree[x]['id'] in check_list:
             comic_id = new_comics_short_tree[x]['id']
             comic_title = new_comics_short_tree[x]['title']
             
-            comic_image_non_ssl = new_comics_short_tree[x]['thumbnail']['path']\
+            on_sale_date_raw = new_comics_short_tree[x]['dates']['type'=='onsaleDate']['date'].split('T')
+            on_sale_date = on_sale_date_raw[0]
+
+            comic_image_non_ssl_front = new_comics_short_tree[x]['thumbnail']['path']
+            comic_image_strip_protocol = comic_image_non_ssl_front.split('//')
+            
+            comic_image = 'https://' + comic_image_strip_protocol[1]\
             + '/portrait_large.'\
             + new_comics_short_tree[x]['thumbnail']['extension']
-            comic_image_strip_protocol = comic_image_non_ssl.split('//')
-            comic_image = 'https://' + comic_image_strip_protocol[1]
+            
+            comic_image_fs = 'https://' + comic_image_strip_protocol[1]\
+            + '/detail.'\
+            + new_comics_short_tree[x]['thumbnail']['extension']
             
             comic_series_uri = new_comics_short_tree[x]['series']['resourceURI']
             comic_series_uri_split = comic_series_uri.split('/')
@@ -48,11 +56,12 @@ def main():
             new_entry = { "comic_id" : comic_id, 
                           "comic_series_id" : comic_series_id,
                           "comic_title" : comic_title,
-                          "cw_add_db" : cw_add_db,
-                          "comic_image" : comic_image }
+                          "on_sale_date" : on_sale_date,
+                          "comic_image" : comic_image,
+                          "comic_image_fs" : comic_image_fs}
                         
-            new_comics.insert_one(new_entry)
-            x+=1
+            comics.insert_one(new_entry)
+        x+=1
 
     
 main()
